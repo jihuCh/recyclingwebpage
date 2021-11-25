6c40e71a75703431442d5db0b6855a7d9c826847
jihuCh <d3011666@gmail.com>
2021-11-26 00:12:17 +0900

    complete project

--- routes/result.js
+++ routes/result.js
@@ -4,7 +4,13 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   var resultValue = req.query.resultValue
-  res.render('result', {resultValue : resultValue});
+  if(resultValue > 60) {
+    res.render('result1', {resultValue : resultValue});
+  } else if(resultValue >= 40 && resultValue <= 60) {
+    res.render('result2', {resultValue : resultValue});
+  } else if(resultValue < 40) {
+    res.render('result3', {resultValue : resultValue});
+  }
 });
 
 module.exports = router;
